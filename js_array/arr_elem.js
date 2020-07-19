dd9fc7b31711f12d50cb877accf5508f89e2251e
Md. Sharfuzzaman <[msharfuzzaman9@gmail.com]>
2020-07-19 07:27:48 +0600

    JavaScript Array

---
+++ js_array/arr_elem.js
@@ -0,0 +1,12 @@
+/* 
+    @Title: Traverse Array Elements
+
+*/
+
+
+const arr = [1, 2, 3, 4, 5];
+
+const n = arr[3];
+const m = arr[2];
+const x = 1, y = 0;
+console.log(m, n, arr[y], arr[x] + arr[y], arr[x + y + 1]);
